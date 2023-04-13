0c75b4d17d516ac38b1ed29982c15b3c849061b1
Francisco Silva <1221518@isep.ipp.pt>
2023-04-13 18:51:04 +0100

    💄 Adicionada animação para Carousel

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,28 @@ module.exports = {
       colors: {
         primary: "#29565F",
       },
+      animation: {
+        "move-in-right": "move-in-right-left-right 600ms ease-in-out",
+      },
+      keyframes: {
+        "move-in-right-left-right": {
+          "0%": {
+            transform: "translateX(0) scale(1)",
+          },
+          "49%": {
+            transform: "translateX(-200%) scale(0.9)",
+          },
+          "50%": {
+            transform: "translateX(200%) scale(0.9)",
+          },
+          "100%": {
+            transform: "translateX(0) scale(1)",
+          },
+        },
+      },
     },
   },
   plugins: [],
 };
+
+// cubic-bezier(.24,-0.29,.9,1.24)
