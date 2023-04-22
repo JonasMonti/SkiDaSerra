879eb437637237d8c746c303774250b2cd00dd52
João Monteiro <1221521@isep.ipp.pt>
2023-04-22 12:07:30 +0100

    Fizemos tanta coisa que nem sei! Backoffice, design, dados TUDO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), react()],
   output: "server",
   adapter: vercel()
 });
